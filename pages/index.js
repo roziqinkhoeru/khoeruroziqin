b489b1b9cec03b3dbb25a26d4062df50c8a86d6c
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-08-13 17:35:09 +0700

    initial tailwind dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Main from "../components/Main";
 
 export default function Home() {
   return (
@@ -9,9 +10,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main></main>
-
-      <footer></footer>
+      <Main />
     </div>
   );
 }
