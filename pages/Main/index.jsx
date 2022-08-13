412aac77d3c0cee9b0cb91a020aa5a634a4a68af
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-08-14 00:06:23 +0700

    finish responsive

--- pages/Main/index.jsx
+++ pages/Main/index.jsx
@@ -4,8 +4,8 @@ import MediaLink from "./components/MediaLink";
 const Main = () => {
   return (
     <>
-      <main className='w-full h-screen flex items-center justify-center pt-12'>
-        <div className='flex flex-col mx-3 sm:mx-16 md:mx-20'>
+      <main className='w-full h-screen min-h-[42rem] sm:min-h-0 flex items-center justify-center pt-8 sm:pt-12'>
+        <div className='flex flex-col py-6 mx-6 sm:mx-16 md:mx-20 max-w-screen-2xl'>
           <h1 className='mb-6 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
             Made With Experience
           </h1>
--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       </Head>
       <Navbar />
       <Main />
-      <div className='absolute right-12 bottom-8'>
+      <div className='fixed right-6 sm:right-12 bottom-6 sm:bottom-8'>
         <button
           onClick={() => toMail()}
           className='flex justify-center items-center bg-slate-600 hover:bg-slate-500 w-14 h-14 rounded-full'
